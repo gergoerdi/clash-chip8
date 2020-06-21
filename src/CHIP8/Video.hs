89dc6e2f4c8a526fe609a846776364b2d8b46379
Dr. ERDI Gergo <gergo@erdi.hu>
2020-06-21 14:13:17 +0800

    Rewrite video subsystem to use a more efficient shift register to shift out the current row's pixels

--- src/CHIP8/Video.hs
+++ src/CHIP8/Video.hs
@@ -10,6 +10,7 @@ import RetroClash.Utils
 import Data.Maybe
 import Control.Arrow (first)
 import CHIP8.Types
+import Control.Monad (join)
 
 -- | 25 MHz clock, needed for the VGA mode we use.
 createDomain vSystem{vName="Dom25", vPeriod = hzToPeriod 25_175_000}
@@ -23,15 +24,26 @@ video write = (matchDelay rgb False frameEnd, delayVGA vgaSync rgb)
     VGADriver{..} = vgaDriver vga640x480at60
     frameEnd = isFalling False (isJust <$> vgaY)
 
-    vgaX' = fromSignal $ scale (SNat @9) . center @576 $ vgaX
-    vgaY' = fromSignal $ scale (SNat @9) . center @288 $ vgaY
+    vgaX' = scale (SNat @9) . center @(9 * 64) $ vgaX
+    vgaY' = scale (SNat @9) . center @(9 * 32) $ vgaY
 
-    rgb = maybe border monochrome <$> pixel
+    rgb = maybe border palette <$> pixel
 
-    row = delayedBlockRam1 ClearOnReset (SNat @32) 0x00 (fromMaybe 0 <$> vgaY') (fmap (first bitCoerce) <$> fromSignal write)
-    pixel = do
-        row <- enable (delayI False $ isJust <$> vgaY') row
-        x <- delayI Nothing vgaX'
-        pure $ (!) <$> row <*> ((63 -) <$> x)
+    lineStart = isRising False (isJust <$> vgaX')
+    newX = vgaX' ./=. register Nothing vgaX'
+
+    addr = join <$> enable lineStart vgaY'
+    load = blockRam1 ClearOnReset (SNat @32) 0x00 (fromMaybe 0 <$> addr) (fmap (first bitCoerce) <$> write)
+
+    row = register 0 $
+          mux (register False $ isJust <$> addr) load $
+          mux (register False newX) ((`shiftL` 1) <$> row) $
+          row
+    pixel = unsafeFromSignal @_ @_ @2 $ enable (register False . register False $ isJust <$> vgaX' .&&. isJust <$> vgaY') $
+            msb <$> row
 
     border = (0x30, 0x30, 0x50)
+
+    palette :: Bit -> (Unsigned 8, Unsigned 8, Unsigned 8)
+    palette 0 = minBound
+    palette 1 = maxBound
