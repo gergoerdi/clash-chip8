2ece1834613180ee65bdf91d9581eb43adb3fdb4
Dr. ERDI Gergo <gergo@erdi.hu>
2020-07-10 13:12:08 +0800

    Update to the book version

--- src/CHIP8/Video.hs
+++ src/CHIP8/Video.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE RecordWildCards, NumericUnderscores, ApplicativeDo #-}
+{-# LANGUAGE MonomorphismRestriction #-}
 module CHIP8.Video where
 
 import Clash.Prelude
 import RetroClash.VGA
 import RetroClash.Video
-import RetroClash.Delayed
 import RetroClash.Utils
+import RetroClash.Delayed
 
 import Data.Maybe
 import Control.Arrow (first)
@@ -19,33 +20,31 @@ video
     :: (HiddenClockResetEnable Dom25)
     => Signal Dom25 (Maybe (VidY, VidRow))
     -> (Signal Dom25 Bool, VGAOut Dom25 8 8 8)
-video write = (matchDelay rgb False frameEnd, delayVGA vgaSync rgb)
+video write = (frameEnd, delayVGA vgaSync rgb)
   where
     VGADriver{..} = vgaDriver vga640x480at60
     frameEnd = isFalling False (isJust <$> vgaY)
 
-    vgaX' = fromSignal $ scale (SNat @9) . center @(9 * 64) $ vgaX
-    vgaY' = fromSignal $ scale (SNat @9) . center @(9 * 32) $ vgaY
+    vgaX' = fromSignal $ scale @64 (SNat @10) vgaX
+    vgaY' = fromSignal $ scale @32 (SNat @10) . center $ vgaY
 
     rgb = maybe border palette <$> pixel
 
-    lineStart = isRisingD False $ (isJust <$> vgaX')
-    newY = changedD Nothing vgaY'
-    newX = changedD Nothing vgaX'
-    visible = isJust <$> vgaX' .&&. isJust <$> vgaY'
-
-    addr = mux newY vgaY' (pure Nothing)
-    write' = fmap (first bitCoerce) <$> fromSignal write
-    load = delayedBlockRam1 ClearOnReset (SNat @32) 0x00 (fromMaybe 0 <$> addr) write'
-
-    row = delayedRegister 0 $ \row ->
-        mux (delayI False $ isJust <$> addr) load $
-        mux (delayI False $ not <$> lineStart .&&. newX) ((`rotateL` 1) <$> row) $
-        row
-    pixel = enable (delayI False visible) $ msb <$> row
-
     border = (0x30, 0x30, 0x50)
 
-    palette :: Bit -> (Unsigned 8, Unsigned 8, Unsigned 8)
     palette 0 = (0x00, 0x00, 0x00)
-    palette 1 = (0xff, 0xff, 0xff)
+    palette 1 = (0xff, 0xcc, 0x33)
+
+    pixel = enable (delayI False visible) $ msb <$> row
+    visible = isJust <$> vgaX' .&&. isJust <$> vgaY'
+
+    address = bitCoerce <$> mux lineStart vgaY' (pure Nothing)
+    load = delayedRam (blockRam1 ClearOnReset (SNat @32) 0) (fromMaybe 0 <$> address) (fromSignal write)
+
+    lineStart = liftD (isRising False) $ (isJust <$> vgaX')
+    newX = liftD (changed Nothing) vgaX'
+
+    row = delayedRegister 0 $ \row ->
+        mux (delayI False $ isJust <$> address) load $
+        mux (delayI False newX) ((`shiftL` 1) <$> row) $
+        row
