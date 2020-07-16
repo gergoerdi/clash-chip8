e2aa9aeba0210ad440249092cbd693ae8f0e0a26
Dr. ERDI Gergo <gergo@erdi.hu>
2020-07-16 21:57:22 +0800

    Move memory address decoder to its own module

--- src/CHIP8/LogicBoard.hs
+++ src/CHIP8/LogicBoard.hs
@@ -8,37 +8,13 @@ import Control.Arrow (second)
 import CHIP8.Types
 import CHIP8.CPU
 import CHIP8.Font
+import CHIP8.Memory
 
 import Data.Functor.Barbie
 import Barbies.Bare
 
-import Debug.Trace
-
-data Mem dom a d
-    = RAM (Signal dom a -> Signal dom (Maybe (a, d)) -> Signal dom d)
-    | ROM (Signal dom a -> Signal dom d)
-
-data MemSpec dom a d
-    = UpTo a (Mem dom a d) (MemSpec dom a d)
-    | Default (Mem dom a d)
-
-memoryMap
-    :: (Ord a, Num a, HiddenClockResetEnable dom)
-    => MemSpec dom a d
-    -> Signal dom a
-    -> Signal dom (Maybe d)
-    -> Signal dom d
-memoryMap mems addr wr = go mems addr
-  where
-    go mems addr = case mems of
-        UpTo lim mem mems -> mux (register False $ addr .< lim) (connect mem) (go mems $ subtract lim <$> addr)
-        Default mem -> connect mem
-      where
-        connect (RAM ram) = ram addr (packWrite <$> addr <*> wr)
-        connect (ROM rom) = rom addr
-
 logicBoard
-    :: forall dom. (HiddenClockResetEnable dom)
+    :: (HiddenClockResetEnable dom)
     => FilePath
     -> Signal dom Bool
     -> Signal dom KeypadState
@@ -48,16 +24,10 @@ logicBoard programFile tick keyState = vidOut
     CPUOut{..} = cpu CPUIn{..}
     vidOut = packWrite <$> _vidAddr <*> _vidWrite
 
-    memRead = memoryMap mems _memAddr _memWrite
-      where
-        mems = UpTo 0x0200 (ROM fontROM) $
-               Default (RAM ram)
+    memRead = memory memSpec _memAddr _memWrite
+    vidRead = blockRam1 ClearOnReset (SNat @32) 0 _vidAddr vidOut
 
     -- Use TH to force `hexDigits` into normal form, otherwise Clash synthesis fails
-    fontROM = romPow2 $(lift hexDigits) . fmap fromIntegral
-
-    ram addr wr = unpack <$> blockRamFile (SNat @(0x1000 - 0x200)) programFile addr wr'
-      where
-        wr' = fmap (second pack) <$> wr
-
-    vidRead = blockRam1 ClearOnReset (SNat @32) 0 _vidAddr vidOut
+    memSpec =
+        UpTo    0x200 (ROM $ rom $(lift hexDigits)) $
+        Default       (RAM $ packRAM $ blockRamFile (SNat @(0x1000 - 0x200)) programFile)
