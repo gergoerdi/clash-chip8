5b6ea1335a7794816907cfcf94320493dc670921
Dr. ERDI Gergo <gergo@erdi.hu>
2020-10-09 20:45:57 +0800

    Upgrade clash-shake

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 import Clash.Shake
+import Clash.Shake.Xilinx
 
 import Development.Shake
 import Development.Shake.Command
@@ -15,33 +16,38 @@ import Data.Maybe (fromMaybe)
 import Control.Monad.Reader
 import Control.Monad.Trans.Class
 
-clashProject = ClashProject
-    { projectName = "CHIP8"
-    , clashModule = "CHIP8"
-    , clashTopName = "topEntity"
-    , topName = "Top"
-    , clashFlags =
-        [ "-i../retroclash-lib/src"
-        , "-Wno-partial-type-signatures"
-        , "-fclash-inline-limit=600"
-        ]
-    , buildDir = "_build"
-    , clashDir = "clash-syn"
-    }
+outDir :: FilePath
+outDir = "_build"
 
 main :: IO ()
-main = clashShake clashProject $ do
-    ClashProject{..} <- ask
-    let synDir = buildDir </> clashDir
+main = shakeArgs shakeOptions{ shakeFiles = outDir } $ do
+    useConfig "build.mk"
 
-    kit@ClashKit{..} <- clashRules Verilog "src" $ do
-        need [buildDir </> "image.bin"]
+    phony "clean" $ do
+        putNormal $ "Cleaning files in " <> outDir
+        removeFilesAfter outDir [ "//*" ]
 
-    -- xilinxISE kit papilioPro "target/papilio-pro" "papilio-pro"
-    -- xilinxISE kit papilioOne "target/papilio-one" "papilio-one"
-    xilinxVivado kit nexysA750T "target/nexys-a7-50t" "nexys-a7-50t"
+    outDir </> "image.bin" %> \out -> do
+        imageFile <- fromMaybe "roms/hidden.ch8" <$> getConfig "IMAGE"
+        binImage (Just $ 0x1000 - 0x0200) imageFile out
 
-    lift $ do
-      buildDir </> "image.bin" %> \out -> do
-          imageFile <- fromMaybe "roms/hidden.ch8" <$> getConfig "IMAGE"
-          binImage (Just $ 0x1000 - 0x0200) imageFile out
+    kit@ClashKit{..} <- clashRules outDir "clash" Verilog
+        [ "src" ]
+        "CHIP8"
+        [ "-Wno-partial-type-signatures"
+        , "-fclash-inline-limit=600"
+        ] $
+        need [outDir </> "image.bin"]
+    phony "clashi" $ clash ["--interactive", unBuildDir "src/CHIP8.hs"]
+
+    let targets =
+            [ ("nexys-a7-50t", xilinxVivado nexysA750T)
+            -- , ("papilio-pro", xilinxISE papilioPro)
+            -- , ("papilio-one", xilinxISE papilioOne)
+            ]
+
+    forM_ targets $ \(name, synth) -> do
+        SynthKit{..} <- synth kit (outDir </> name) ("target" </> name) "Top"
+
+        mapM_ (uncurry $ nestedPhony name) $
+          ("bitfile", need [bitfile]):phonies
